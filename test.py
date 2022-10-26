d58531251ed2fa12cca210958ada769f843e2a7d
Leandro Garber <leandrogarber@gmail.com>
2022-10-26 17:04:40 -0300

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+from ChaFile import *
+from glob import glob
+
+chaPaths = glob("/home/macramole/Code/ciipme/aclew/highvolResults/listos_cha/*.cha")
+# chaPath = "/home/macramole/Code/ciipme/aclew/highvolResults/listos_cha/donatow-a1-nsm.elan.cha"
+# chaPath = "/home/macramole/Code/ciipme/corpusESPON/longi_audio1/codificados/codificados bullets elan/alma-a1-nsb.cha"
+# cha = ChaFile( chaPath, language=LANGUAGE_SPANISH )
+
+for c in chaPaths:
+    cha = ChaFile( c, language=LANGUAGE_SPANISH )
+    cha.countUtterances()
+
